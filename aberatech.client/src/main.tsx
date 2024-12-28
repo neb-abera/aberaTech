9fc81b40d1a59023b9e54c1c70a85f5229bf08ad
Neb Abera <29741322+neb-abera@users.noreply.github.com>
2024-12-27 23:46:56 -0600

    prettier ignore .md

--- aberatech.client/src/main.tsx
+++ aberatech.client/src/main.tsx
@@ -1,8 +1,8 @@
 /* eslint-disable  @typescript-eslint/no-non-null-assertion */
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
-import {BrowserRouter} from 'react-router';
-import {StyledEngineProvider} from '@mui/material/styles';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router';
+import { StyledEngineProvider } from '@mui/material/styles';
 import './index.css';
 import App from './App.tsx';
 
