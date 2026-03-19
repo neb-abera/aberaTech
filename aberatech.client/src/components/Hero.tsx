58959eabdd71f4798610a49b5589f74798ec1c5c
Neb Abera <29741322+neb-abera@users.noreply.github.com>
2026-03-18 20:20:37 -0600

    IMP 1 - update link to transition guide, add docker instructions to readme, update social media links (#6)
    * fix link to abera.tech/transition instead of repo
    
    * update docker instructions, link to transition guide
    
    * Update docker instructions in readme
    
    * Update workflow to include master branch
    
    * update phone icon, add instagram

--- aberatech.client/src/components/Hero.tsx
+++ aberatech.client/src/components/Hero.tsx
@@ -4,8 +4,9 @@ import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import PhoneIphoneIcon from '@mui/icons-material/PhoneIphone';
+import InstagramIcon from '@mui/icons-material/Instagram';
 
 export default function Hero() {
   return (
@@ -57,13 +58,27 @@ export default function Hero() {
           </Typography>
           <Typography
             sx={{
-              textAlign: 'center',
+              textAlign: 'left',
               color: 'text.primary'
             }}
           >
-            I am a Software Engineer in Washington D.C. I am an avid reader, and I love learning how to solve problems that
-            require an interdisciplinary approach. If you have any book recommendations please send them my way! I am
-            currently working on projects in C++, TypeScript, and C#.
+            I really enjoy detail-oriented problem-solving, and I have years of experience leading teams of technical professionals. I specialize in building secure embedded systems. I have worked on large-scale teams and have independently developed novel research projects to support RF Engineering / Digital Signal Processing programs.
+          </Typography>
+          <Typography
+            sx={{
+              textAlign: 'left',
+              color: 'text.primary'
+            }}
+          >
+            I am currently working on projects in C++, Python, VHDL in Vivado, and web development (TypeScript/React/HTML/CSS + C# using .NET). When I used to have free time... I dedicated it to Mixed Martial Arts, mostly Brazilian Jiu-Jitsu and Judo.
+          </Typography>
+          <Typography
+              sx={{
+                textAlign: 'left',
+                color: 'text.primary'
+              }}
+            >
+            I am an avid reader, and I love learning how to solve problems that require an interdisciplinary approach. If you have any book recommendations please send them my way!
           </Typography>
           <Stack direction="row" spacing={1} useFlexGap sx={{ justifyContent: 'left', color: 'text.secondary' }}>
             <IconButton
@@ -91,7 +106,16 @@ export default function Hero() {
               aria-label="Google"
               sx={{ alignSelf: 'center' }}
             >
-              <CalendarMonthIcon />
+              <PhoneIphoneIcon />
+            </IconButton>
+            <IconButton
+              color="inherit"
+              size="large"
+              href="https://www.instagram.com/neb_abera"
+              aria-label="Instagram"
+              sx={{ alignSelf: 'center' }}
+            >
+              <InstagramIcon />
             </IconButton>
           </Stack>
         </Stack>
